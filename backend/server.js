74e9dbcd4c13f178552ddb99a22f07d6f8537f88
Sarav <Saravjeetofficial@gmail.com>
2026-05-05 09:19:51 +0530

    added database connectivity and userModel

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import app from "./src/app.js";
 import config from "./src/config/config.js";
+import connectDB from "./src/config/db.js";
 
 const PORT = config.PORT;
 
+connectDB();
+
 app.listen(PORT, ()=>{
-    console.log("Server running ", PORT);
+    console.log("Server running on: ", PORT);
 })
