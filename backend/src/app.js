8334c6286b30ab5125209df1b4d4a42fd7cd947b
Sarav <Saravjeetofficial@gmail.com>
2026-05-08 19:50:07 +0530

    installed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,13 @@ import authRouter from "./routes/auth.routes.js"
 import chatRouter from "./routes/chat.routes.js"
 const app = express();
 
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
