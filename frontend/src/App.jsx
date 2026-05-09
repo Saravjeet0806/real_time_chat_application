f52088f7d007b941f3c2c6791ee1157df2e7c45b
Sarav <Saravjeetofficial@gmail.com>
2026-05-09 14:17:02 +0530

    react router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import Register from './components/Register'
+import { Route, Routes } from 'react-router'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <>
-    <Register/>
+     <Routes>
+        
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+
+      </Routes>
     </>
   )
 }
