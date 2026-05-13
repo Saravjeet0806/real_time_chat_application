294a51564da9ba9110f658689ec2e85088851758
Saravjeet <saravjeetofficial@gmail.com>
2026-05-13 21:43:59 +0530

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import HomePage from './pages/Homepage'
 
 const App = () => {
   return (
     <>
      <Routes>
-        
+        <Route path="/" element={<HomePage/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login/>} />
 
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Homepage = () => {
+  return (
+    <div className='bg-linear-to-tl from-red-500 via-orange-400 to-indigo-600 min-h-screen w-full'>
+       <Navbar/>
+    </div>
+  )
+}
+
+export default Homepage
